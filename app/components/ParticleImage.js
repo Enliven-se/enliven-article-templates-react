39cf556d56472022957b385fabc390609fd2f56e
klokie <github@klokie.com>
2016-11-18 12:29:54 +0100

    removed "container" class from particles

--- app/components/ParticleImage.js
+++ app/components/ParticleImage.js
@@ -2,7 +2,7 @@ const React = window.React = require('react')
 
 const ParticleImage = React.createClass({
   render: function() {
-    return <div className="container">{this.props.particleName}</div>
+    return <div className="">{this.props.particleName}</div>
   }
 })
 
--- app/components/ParticlePreamble_intro_.js
+++ app/components/ParticlePreamble_intro_.js
@@ -2,7 +2,7 @@ const React = window.React = require('react')
 
 const ParticlePreamble_intro_ = React.createClass({
   render: function() {
-    return <div className="container">{this.props.particleName}</div>
+    return <div className="">{this.props.particleName}</div>
   }
 })
 
