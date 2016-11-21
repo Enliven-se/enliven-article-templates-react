3af24fa499a64e33d25ab46bdb3d89d7b98c6803
klokie <github@klokie.com>
2016-11-21 14:12:02 +0100

    fixed intro text

--- app/data/MockShortInterview.js
+++ app/data/MockShortInterview.js
@@ -11,6 +11,11 @@ const MockShortInterview = {
     }, {
       type: 'text',
       content: {
+        html: 'Created by a group of friends who preserve 55 acres of hidden forest in Upstate New York began as a scrapbook to collect inspiration for their building projects. Created by a group of friends who preserve 55 acres of hidden forest in Upstate.',
+      }
+    }, {
+      type: 'text',
+      content: {
         html: 'If you had to live your life over again, what one thing would you change? This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is \“packed\” in boxes is usual for such a grid-based design; however, the alignment of the boxes makes the design literally stand out. This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is \“packed\” in boxes is usual for such a grid-based design; however, the alignment of the boxes makes the design literally stand out. This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is \“packed\” in boxes is usual for such a grid-based design; however, the alignment of the boxes makes the design literally stand out. This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is \“packed\” in boxes is usual for such a grid-based design; however, the alignment of the boxes makes the design literally.'
       }
     }, {
@@ -45,7 +50,6 @@ const MockShortInterview = {
       html: 'The Different Sides of Charlize'
     }
   },
-  Preamble_intro_: 'Created by a group of friends who preserve 55 acres of hidden forest in Upstate New York began as a scrapbook to collect inspiration for their building projects. Created by a group of friends who preserve 55 acres of hidden forest in Upstate.',
   ContributorList: [
     {
       Avatar: '/images/mock/avatar.png',
