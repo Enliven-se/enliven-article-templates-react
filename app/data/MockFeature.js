35ed96c0d87784d11631367b01c7ba4f6f86c2b5
bisaiahblodgett <bisaiahblodgett@gmail.com>
2016-12-09 16:44:55 +0100

    final fixes to short interview and lookbook

--- app/data/MockFeature.js
+++ app/data/MockFeature.js
@@ -50,7 +50,7 @@ const MockTheWayWePlay = {
   }, {
     'type': 'text',
     'content': {
-      'html': 'Var: Hemma hos Emelie, Gustav och Nico Anaïs i deras vackra lägenhet på Upper East Side i New York.<br/>När: En varm höstdag i november, 2015<br/>Hur: Med en promenad bland New York Brownstones och mysigt häng i drömmarnas boho-lyx lägenhet.<br/>Att få komma hem till Emelie och Nico Anaïs och spendera en stund tillsammans med dessa ljuvliga två och ha staden New York som kuliss känns nära på filmiskt. Vi vandrar runt i lägenheten som är en direkt spegel av det Emelie själv andas och lever och som det egna märket Leontine personifierar – en bohemisk fri själ med. Emelie Törling  är grundare och designer på Leontine samt sångerska. Läs hennes blogg här!<br/>Tack Emelie och Nico Anaïs för att vi fick komma!'
+      'html': '<strong class="text-color-gold">Var:</strong> Hemma hos Emelie, Gustav och Nico Anaïs i deras vackra lägenhet på Upper East Side i New York.<br/><br/><strong class="text-color-gold">När:</strong> En varm höstdag i november, 2015<br/><br/><strong class="text-color-gold">Hur:</strong> Med en promenad bland New York Brownstones och mysigt häng i drömmarnas boho-lyx lägenhet.<br/>Att få komma hem till Emelie och Nico Anaïs och spendera en stund tillsammans med dessa ljuvliga två och ha staden New York som kuliss känns nära på filmiskt. Vi vandrar runt i lägenheten som är en direkt spegel av det Emelie själv andas och lever och som det egna märket Leontine personifierar – en bohemisk fri själ med. Emelie Törling  är grundare och designer på Leontine samt sångerska. Läs hennes blogg här!<br/><br/><strong class="text-color-gold">Tack Emelie och Nico Anaïs för att vi fick komma!</strong>'
     }
   }, {
     'type': 'image',
@@ -62,12 +62,12 @@ const MockTheWayWePlay = {
   }, {
     'type': 'text',
     'content': {
-      'html': 'Berätta om Leontine.<br/><br/>Jag och min man Gustaf bodde i Laurel Canyon i Los Angeles 2012 och jag kunde aldrig riktigt hitta boots eller accessorer som jag ville bära, så jag började tillverka saker till mig själv. Första dagen på stan i LA med bootsen på frågade femton personer mig var de gick att köpa. Jag gav min mailadress och gick hem och startade mitt märke. Jag började göra boots och smycken i återvunnen metall och gjorde om vintage smycken. Jag designade en handgjord klädkollektion på 10 plagg som såldes slut direkt och jag skulle precis lansera en smyckeskollektion, men allt lades på is då min dotter valda att komma ut två månader för tidigt.'
+      'html': '<strong>Berätta om Leontine.</strong><br/><br/>Jag och min man Gustaf bodde i Laurel Canyon i Los Angeles 2012 och jag kunde aldrig riktigt hitta boots eller accessorer som jag ville bära, så jag började tillverka saker till mig själv. Första dagen på stan i LA med bootsen på frågade femton personer mig var de gick att köpa. Jag gav min mailadress och gick hem och startade mitt märke. Jag började göra boots och smycken i återvunnen metall och gjorde om vintage smycken. Jag designade en handgjord klädkollektion på 10 plagg som såldes slut direkt och jag skulle precis lansera en smyckeskollektion, men allt lades på is då min dotter valda att komma ut två månader för tidigt.'
     }
   }, {
     'type': 'text',
     'content': {
-      'html': 'Vem bär Leontine?<br/><br/>"Leontinekvinnan" är självständig, självsäker och orädd. Hon gör det hon vill och tror på, vad det än är som gör henne lycklig, utan en tanke på vart det ska leda. Hon är en bohemisk och fri själ med en stark stil och hon inspirerar andra att leva som hon. Märket är en tydlig blandning av eklektisk lyx-rocknroll och elegans. En perfekt grå sliten tisha tillsammans med ett par guldpaljettbyxor. Jag har gjort boots till Kate Moss, Englands superstylist Katy England och artisten Cat Power. Superkul!'
+      'html': '<strong>Vem bär Leontine?</strong><br/><br/>"Leontinekvinnan" är självständig, självsäker och orädd. Hon gör det hon vill och tror på, vad det än är som gör henne lycklig, utan en tanke på vart det ska leda. Hon är en bohemisk och fri själ med en stark stil och hon inspirerar andra att leva som hon. Märket är en tydlig blandning av eklektisk lyx-rocknroll och elegans. En perfekt grå sliten tisha tillsammans med ett par guldpaljettbyxor. Jag har gjort boots till Kate Moss, Englands superstylist Katy England och artisten Cat Power. Superkul!'
     }
   }, {
     'type': 'image',
@@ -82,7 +82,7 @@ const MockTheWayWePlay = {
   }, {
     'type': 'text',
     'content': {
-      'html': 'Berätta om första tiden med Nico Anaïs.'
+      'html': '<strong>Berätta om första tiden med Nico Anaïs.</strong>'
     },
     'classes': 'text-color-gold text-uppercase'
   }, {
@@ -93,7 +93,7 @@ const MockTheWayWePlay = {
   }, {
     'type': 'text',
     'content': {
-      'html': 'Hur var det när hon äntligen fick komma hem?<br/><br/>Jag var livrädd och allt var som i en dimma den första tiden. Vi sov inte. Vi hade levt med henne på sjukhuset och var trygga med att hon låg vid en monitor som pep om hennes hjärta glömde slå ett slag. Att se henne utan sladdar var läskigt. Anaïs hade kolik i tre månader och jag gick runt med öronproppar och med sovmask i pannan, så när hon väl somnade under dagen la jag mig bredvid henne och försökte sova en stund. Jag försökte lära henne att amma och det kunde ta upp till tre timmar varje gång. Jag fick väcka henne på natten med en våt trasa på pannan och blåsa på henne för att hon skulle vakna till lite och amma. Förra sommaren var jag i Sverige en längre tid och fick avlastning av mina underbara föräldrar, då började jag få tillbaka gnistan. Gustav var det bästa stödet, men började på ett nytt jobb i samma veva så det var oerhört tufft för honom också. När jag började förstå hennes signaler var det en obeskrivlig lycka. Jag och Gustaf grät båda två när hon log för första gången. Det var så stort för oss. Hon hade växt och var ok.'
+      'html': '<strong>Hur var det när hon äntligen fick komma hem?</strong><br/><br/>Jag var livrädd och allt var som i en dimma den första tiden. Vi sov inte. Vi hade levt med henne på sjukhuset och var trygga med att hon låg vid en monitor som pep om hennes hjärta glömde slå ett slag. Att se henne utan sladdar var läskigt. Anaïs hade kolik i tre månader och jag gick runt med öronproppar och med sovmask i pannan, så när hon väl somnade under dagen la jag mig bredvid henne och försökte sova en stund. Jag försökte lära henne att amma och det kunde ta upp till tre timmar varje gång. Jag fick väcka henne på natten med en våt trasa på pannan och blåsa på henne för att hon skulle vakna till lite och amma. Förra sommaren var jag i Sverige en längre tid och fick avlastning av mina underbara föräldrar, då började jag få tillbaka gnistan. Gustav var det bästa stödet, men började på ett nytt jobb i samma veva så det var oerhört tufft för honom också. När jag började förstå hennes signaler var det en obeskrivlig lycka. Jag och Gustaf grät båda två när hon log för första gången. Det var så stort för oss. Hon hade växt och var ok.'
     }
   }, {
     'type': 'image',
@@ -108,7 +108,7 @@ const MockTheWayWePlay = {
   }, {
     'type': 'text',
     'content': {
-      'html': 'Största skillnaden med att bo i New York jämfört med Sverige?<br/><br/>En stor skillnad är att du kan göra vad du vill här och ingen annan har egentligen med det att göra. Det finns inget lagom. Här finns så många olika kulturer och alla lever på olika sätt. Det finns en frihet här kring hur man vill leva och det är överlag en väldigt stöttande omgivning. Vill du uppfostra ditt barn till vegan, ha vagnen framåt, starta en ny underlig business eller kanske konvertera till en annan religion så är det ingen som höjer på ögonbrynen. Det spelar ingen roll vad det är, alla säger bara "good for you, good luck, let me now if you need any help". En enorm skillnad är också självklart att det i Sverige finns en fantastisk grundtrygghet. I USA är det ditt liv, ditt ansvar. Ingen bryr sig egentligen om ditt liv går åt skogen. Det egna ansvaret är bra för den som har möjlighet att ta sig vidare i livet. Värre för den som inte kan och där vinner Sverige alltid. I USA finns det enorma klyftor. I Sverige är grundmentaliteten att ingen ska hamna utanför, det är så unikt och fantastiskt. En blandning vore ultimat tror jag. Man ska våga satsa stort och kämpa för sina drömmar men har man inte fötts med möjligheter och verkligen behöver hjälp så borde samhället finnas där med sjukvård och en "madrass" att landa på. Framförallt för barnen och de som far illa.<br/><br/>När är du som lyckligast?<br/><br/>När det är lördag och vi vaknar i vårt hus i Amagansett dit vi åkt sent kvällen innan. Gustaf har tagit upp Anaïs som vaknat tidigt och jag får sova ut. När jag sedan kommer ner till köket vid tiotiden på förmiddagen så har han dukat fram kaffe och färskt bröd och tänt en massa ljus. Det är det bästa jag vet.'
+      'html': '<strong>Största skillnaden med att bo i New York jämfört med Sverige?</strong><br/><br/>En stor skillnad är att du kan göra vad du vill här och ingen annan har egentligen med det att göra. Det finns inget lagom. Här finns så många olika kulturer och alla lever på olika sätt. Det finns en frihet här kring hur man vill leva och det är överlag en väldigt stöttande omgivning. Vill du uppfostra ditt barn till vegan, ha vagnen framåt, starta en ny underlig business eller kanske konvertera till en annan religion så är det ingen som höjer på ögonbrynen. Det spelar ingen roll vad det är, alla säger bara "good for you, good luck, let me now if you need any help". En enorm skillnad är också självklart att det i Sverige finns en fantastisk grundtrygghet. I USA är det ditt liv, ditt ansvar. Ingen bryr sig egentligen om ditt liv går åt skogen. Det egna ansvaret är bra för den som har möjlighet att ta sig vidare i livet. Värre för den som inte kan och där vinner Sverige alltid. I USA finns det enorma klyftor. I Sverige är grundmentaliteten att ingen ska hamna utanför, det är så unikt och fantastiskt. En blandning vore ultimat tror jag. Man ska våga satsa stort och kämpa för sina drömmar men har man inte fötts med möjligheter och verkligen behöver hjälp så borde samhället finnas där med sjukvård och en "madrass" att landa på. Framförallt för barnen och de som far illa.<br/><br/><strong>När är du som lyckligast?</strong><br/><br/>När det är lördag och vi vaknar i vårt hus i Amagansett dit vi åkt sent kvällen innan. Gustaf har tagit upp Anaïs som vaknat tidigt och jag får sova ut. När jag sedan kommer ner till köket vid tiotiden på förmiddagen så har han dukat fram kaffe och färskt bröd och tänt en massa ljus. Det är det bästa jag vet.'
     }
   }, {
     'type': 'image',
@@ -123,7 +123,7 @@ const MockTheWayWePlay = {
   }, {
     'type': 'text',
     'content': {
-      'html': 'Vad är du rädd för?<br/><br/>Att bli gammal och att mina föräldrar ska bli gamla. Att närstående ska bli sjuka. Att förlora dem omkring mig. Familj och vänner betyder allt för mig.<br/><br/>Vad gör du för att lyfta en jobbig dag?<br/><br/>Rödvin, vänner, telefonsamtal med någon jag saknar, ett skumbad, barnvakt, en yogaklass, en promenad ensam med jazz i öronen. Som tur är finns det många småsaker som rår bot på vardagsbluesen.'
+      'html': '<strong>Vad är du rädd för?</strong><br/><br/>Att bli gammal och att mina föräldrar ska bli gamla. Att närstående ska bli sjuka. Att förlora dem omkring mig. Familj och vänner betyder allt för mig.<br/><br/><strong>Vad gör du för att lyfta en jobbig dag?</strong><br/><br/>Rödvin, vänner, telefonsamtal med någon jag saknar, ett skumbad, barnvakt, en yogaklass, en promenad ensam med jazz i öronen. Som tur är finns det många småsaker som rår bot på vardagsbluesen.'
     }
   }, {
     'type': 'image',
@@ -133,18 +133,18 @@ const MockTheWayWePlay = {
   }, {
     'type': 'text',
     'content': {
-      'html': 'Dina fem bästa New York tips'
+      'html': '<strong class="text-color-gold text-uppercase">Dina fem bästa New York tips</strong>'
     },
     'classes': 'text-color-gold text-uppercase'
   }, {
     'type': 'text',
     'content': {
-      'html': "Indochine – Fransk-vietnamesisk middagsrestaurang, bra för alla tillfällen. Romantisk middag för två eller stimmig fest med tjugo kompisar.<br/>Kiki's – Mitt nya favoritställe på Lower East Side. Det går inte att boka bord men det är bara att droppa in och vänta i baren. Kiki kommer att seata er. Bästa grekmaten någonsin. Beställ in ost med honung till förrätt. Vin får man i små karaffer, man sitter vackert vid träbord, kulörta lampor, stimmigt och billigt!<br/><br/>Dia Beacon – Ett museum som ligger i staden Beacon, ca 50 min med tåg från Manhattan. Ta tåget från Grand Central Station efter att ni druckit ett glas champagne och ätit ostron på The Grand Central Oyster Bar, och njut av konst av Dan Flavin och Donald Judd i den vackraste konstlokal jag sett. Lovar att det är värt resan. Att åka tåg upstate USA är också en upplevelse. Jag vågar säga att naturen i USA är den vackraste i världen."
+      'html': "<strong>Indochine</strong> – Fransk-vietnamesisk middagsrestaurang, bra för alla tillfällen. Romantisk middag för två eller stimmig fest med tjugo kompisar.<br/><br/><strong>Kiki's</strong> – Mitt nya favoritställe på Lower East Side. Det går inte att boka bord men det är bara att droppa in och vänta i baren. Kiki kommer att seata er. Bästa grekmaten någonsin. Beställ in ost med honung till förrätt. Vin får man i små karaffer, man sitter vackert vid träbord, kulörta lampor, stimmigt och billigt!<br/><br/><strong>Dia Beacon</strong> – Ett museum som ligger i staden Beacon, ca 50 min med tåg från Manhattan. Ta tåget från Grand Central Station efter att ni druckit ett glas champagne och ätit ostron på The Grand Central Oyster Bar, och njut av konst av Dan Flavin och Donald Judd i den vackraste konstlokal jag sett. Lovar att det är värt resan. Att åka tåg upstate USA är också en upplevelse. Jag vågar säga att naturen i USA är den vackraste i världen."
     }
   }, {
     'type': 'text',
     'content': {
-      'html': "What Goes Around Comes Around – Den bästa vintage butiken i NYC, hands down. Svindyrt men om så bara för span så är det värt att gå hit varje gång man är downtown för att bli inspirerad. Här finns allt från trashiga Guns N´Roses tishor till Hermés porslin.<br/><br/>Bemelman's barn – Eftersom vi numera bor Upper East Side så måste jag nämna Bemelman's bar på Carlyle Hotell. Här spelar Woody Allan jazz-piano på söndagar. Baren (och hela hotellet för den delen) är en New York institution."
+      'html': "<strong>What Goes Around Comes Around</strong> – Den bästa vintage butiken i NYC, hands down. Svindyrt men om så bara för span så är det värt att gå hit varje gång man är downtown för att bli inspirerad. Här finns allt från trashiga Guns N´Roses tishor till Hermés porslin.<br/><br/><strong>Bemelman's barn</strong> – Eftersom vi numera bor Upper East Side så måste jag nämna Bemelman's bar på Carlyle Hotell. Här spelar Woody Allan jazz-piano på söndagar. Baren (och hela hotellet för den delen) är en New York institution."
     }
   }, {
     'type': 'image',
@@ -154,7 +154,7 @@ const MockTheWayWePlay = {
   }, {
     'type': 'text',
     'content': {
-      'html': 'Hur får man kärleken att hålla under småbarnsåren?<br/><br/>Ha respekt för varandras känslor. Man kommer alltid känna olika inför saker men sålänge man respekterar och försöker hjälpa varandra så kommer man långt. Prata med varandra om allt så blir det färre missförstånd. Ha egentid. Själv men även tillsammans med din partner. Man får inte glömma bort vem man är och vilka man var innan man fick barn, det är det tuffaste men också det vikigaste. Jag och Gustav har ofta barnvakt och det har vi haft sedan Anaïs var väldigt liten. Det har varit viktigt för oss.<br/><br/>Hur länge planerar ni att stanna i New York?<br/><br/>Vi trivs väldigt bra just nu och har inga planer på att lämna men vi pratar om Paris ibland…'
+      'html': '<strong>Hur får man kärleken att hålla under småbarnsåren?</strong><br/><br/>Ha respekt för varandras känslor. Man kommer alltid känna olika inför saker men sålänge man respekterar och försöker hjälpa varandra så kommer man långt. Prata med varandra om allt så blir det färre missförstånd. Ha egentid. Själv men även tillsammans med din partner. Man får inte glömma bort vem man är och vilka man var innan man fick barn, det är det tuffaste men också det vikigaste. Jag och Gustav har ofta barnvakt och det har vi haft sedan Anaïs var väldigt liten. Det har varit viktigt för oss.<br/><br/><strong>Hur länge planerar ni att stanna i New York?</strong><br/><br/>Vi trivs väldigt bra just nu och har inga planer på att lämna men vi pratar om Paris ibland…'
     }
   }, {
     'type': 'image',
