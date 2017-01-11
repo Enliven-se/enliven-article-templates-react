9940574774b44b68b7a8fa99c814ccc7162b6360
bisaiahblodgett <bisaiahblodgett@gmail.com>
2017-01-11 17:47:43 +0100

    add files for columnist

---
+++ app/data/MockColumnist.js
@@ -0,0 +1,81 @@
+const MockColumnist = {
+  'color_variant': 'gold',
+  'ArticleCategory': {
+    'content': {
+      'html': "Women's Health",
+      'url': '#'
+    },
+    'classes': 'text-color-gold text-uppercase'
+  },
+  'ArticleTitle': {
+    'content': {
+      'html': 'The Most Overlooked Tips About fashion'
+    },
+    'classes': 'text-uppercase'
+  },
+  'DatePublished': {
+    'type': 'text',
+    'content': {
+      'html': '18th November 2016'
+    }
+  },
+
+  'fields': {
+    0: {
+      'type': 'image',
+      'content': {
+        'url': '/images/mock/columnist.jpg'
+      }
+    },
+    1: {
+      'type': 'text',
+      'content': {
+        'html': 'Min man Edin, baby Max 0 år och Lola 3 år, I stockholm gör: Inköpare på HM Home och delägare i butiken Dusty Deco som min man.'
+      }
+    },
+    2: {
+      'type': 'text',
+      'content': {
+        'html': 'If you had to live your life over again, what one thing would you . This  design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is “packed” in boxes is usual for such a grid-based design; however, the  boxes makes. <br/><br/> Boxes makes the design literally stand out. This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is “packed” in boxes is usual for such a grid-based design; however, the alignment of the boxes makes the design literally stand out. This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. <br/><br/> The content is “packed” in boxes is usual for such a grid-based design; however, the alignment of the boxes makes the design literally stand out. This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is “packed” in boxes is usual for such a grid-based design; however, the alignment of the boxes makes the design literally .'
+      }
+    },
+    3: {
+      'type': 'pullquote',
+      'content': {
+        'html': 'No one has completed all of their goals or, for that matter, no regrets.'
+      },
+      'classes': 'text-color-gold hangingindent'
+    },
+    4: {
+      'type': 'text',
+      'content': {
+        'html': 'If you had to live your life over again, what one thing would you . This  design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is “packed” in boxes is usual for such a grid-based design; however, the  boxes makes. <br/><br/> Boxes makes the design literally stand out. This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size. The content is “packed” in boxes is usual for such a grid-based design; however, the alignment of the boxes makes the design literally stand out. This design agency uses a flexible JavaScript-based layout which updates its size depending on the browser window size.'
+      }
+    },
+    5: {
+      'type': 'text',
+      'content': {
+        'html': 'The Way We Play'
+      },
+      'url': '#',
+      'classes': 'text-color-gold text-uppercase'
+    },
+    6: {
+      'type': 'text',
+      'content': {
+        'html': 'Follow',
+      },
+      'url': '#',
+      'classes': 'text-color-gold text-uppercase'
+    },
+    7: {
+      'type': 'text',
+      'content': {
+        'html': 'read more...',
+      },
+      'url': '#',
+      'classes': 'text-color-gold'
+    },
+  }
+}
+module.exports = MockColumnist
