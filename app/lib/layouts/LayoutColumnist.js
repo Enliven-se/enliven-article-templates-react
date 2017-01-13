f3fe3830c955ef108a1b12a2ccc6a3d79f05955f
bisaiahblodgett <bisaiahblodgett@gmail.com>
2017-01-13 12:04:45 +0100

    second picture intensive layout created, data input

---
+++ app/lib/layouts/LayoutColumnist.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+// window.React = React
+
+import Container from '../containers/Container'
+import ArticleHeader from '../containers/ArticleHeader'
+import ContributorList from '../containers/ContributorList'
+import DatePublished from '../components/base/DatePublished'
+import HR from '../components/base/HR'
+import Link from '../components/base/Link'
+import ParticleImage from '../components/particles/ParticleImage'
+import ParticleText from '../components/particles/ParticleText'
+import ParticlePullquote from '../components/particles/ParticlePullquote'
+
+class LayoutColumnist extends React.Component {
+  render() {
+    return (
+      <div className='layout layout-columnist'>
+        <Container fluid classes='panel-1'>
+          <Row className='article-header-row'>
+            <Col md={6} className='left-panel'>
+              <ArticleHeader {...this.props.data}/>
+              <DatePublished {...this.props.data.DatePublished}/>
+            </Col>
+            <Col md={6} className='right-panel'>
+              <Col md={5}>
+                <ParticleImage {...this.props.data.fields[0]}/>
+              </Col>
+              <Col md={7} className='about-columnist'>
+                <div className='follow-button'>
+                  <Link text={this.props.data.fields[6].content.html} url={this.props.data.fields[6].url} classes={this.props.data.fields[6].classes}/>
+                </div>
+                <Link text={this.props.data.fields[5].content.html} url={this.props.data.fields[5].url} classes={this.props.data.fields[5].classes}/>
+                <ParticleText {...this.props.data.fields[1]} />
+                <Link text={this.props.data.fields[7].content.html} url={this.props.data.fields[7].url} classes={this.props.data.fields[7].classes}/>
+              </Col>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12} className='hr-container'>
+              <HR className=''/>
+            </Col>
+          </Row>
+          <Row className='body-text'>
+            <Col sm={12}>
+              <div className='first-text'>
+                <ParticleText {...this.props.data.fields[2]} />
+              </div>
+              <ParticlePullquote {...this.props.data.fields[3]}/>
+              <ParticleText {...this.props.data.fields[4]} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default LayoutColumnist
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,7 @@ import LayoutShort from '../lib/layouts/LayoutShort'
 import LayoutFeature from '../lib/layouts/LayoutFeature'
 import LayoutLookbook from '../lib/layouts/LayoutLookbook'
 import LayoutPictureIntensive from '../lib/layouts/LayoutPictureIntensive'
+import LayoutPictureIntensive2 from '../lib/layouts/LayoutPictureIntensive2'
 import LayoutProductReview from '../lib/layouts/LayoutProductReview'
 
 // mock data
@@ -21,49 +22,54 @@ import MockFeature from '../data/MockFeature'
 import MockLookbook from '../data/MockLookbook'
 import MockProductReview from '../data/MockProductReview'
 import MockPictureIntensive from '../data/MockPictureIntensive'
+import MockPictureIntensive2 from '../data/MockPictureIntensive2'
 
 const data = require('./../includes/data.json')
 
 const switchLayout = function(props) {
-    let ChosenLayout = {},
-        mock = {}
+  let ChosenLayout = {},
+    mock = {}
 
-    switch (props.layout) {
-        case 'ProductReview':
-            ChosenLayout = LayoutProductReview
-            mock = MockProductReview
-            break;
-        case 'PictureIntensive':
-            ChosenLayout = LayoutPictureIntensive
-            mock = MockPictureIntensive
-            break;
-        case 'Lookbook':
-            ChosenLayout = LayoutLookbook
-            mock = MockLookbook
-            break;
-        case 'Feature':
-            ChosenLayout = LayoutFeature
-            mock = MockFeature
-            break;
-        case 'Short':
-            ChosenLayout = LayoutShort
-            mock = MockShort
-            break;
-        case 'Grid':
-            ChosenLayout = GridLayout
-            mock = MockFront
-            sticky = false
-            break;
-        default:
-            ChosenLayout = LayoutFront
-            mock = MockFront
-            is_front = true
-            sticky = false
-    }
+  switch (props.layout) {
+    case 'ProductReview':
+      ChosenLayout = LayoutProductReview
+      mock = MockProductReview
+      break;
+    case 'PictureIntensive':
+      ChosenLayout = LayoutPictureIntensive
+      mock = MockPictureIntensive
+      break;
+    case 'PictureIntensive2':
+      ChosenLayout = LayoutPictureIntensive2
+      mock = MockPictureIntensive2
+      break;
+    case 'Lookbook':
+      ChosenLayout = LayoutLookbook
+      mock = MockLookbook
+      break;
+    case 'Feature':
+      ChosenLayout = LayoutFeature
+      mock = MockFeature
+      break;
+    case 'Short':
+      ChosenLayout = LayoutShort
+      mock = MockShort
+      break;
+    case 'Grid':
+      ChosenLayout = GridLayout
+      mock = MockFront
+      sticky = false
+      break;
+    default:
+      ChosenLayout = LayoutFront
+      mock = MockFront
+      is_front = true
+      sticky = false
+  }
 
-    color_variant = mock.color_variant
-    return <ChosenLayout data = {
-        mock
+  color_variant = mock.color_variant
+  return <ChosenLayout data = {
+    mock
     }
     />
 }
@@ -71,8 +77,8 @@ const switchLayout = function(props) {
 // query string parsing
 let layout = location.search.match(/[&\?]layout=(\w+)/)
 layout = layout && (layout.length > 1) ?
-    layout[1] :
-    ''
+  layout[1] :
+  ''
 
 // global rendering options
 let color_variant = ''
@@ -80,29 +86,29 @@ let sticky = true
 let is_front = false
 
 const content = switchLayout({
-    'layout': layout
+  'layout': layout
 })
 
 // React root
 const mountNode = document.getElementById('app')
 
-ReactDOM.render( <
-        LayoutContainer layout = {
-            layout
-        }
-        navbar_items = {
-            data.navbar_items
-        }
-        color_variant = {
-            color_variant
-        }
-        sticky = {
-            sticky
-        }
-        is_front = {
-            is_front
-        }
-        > {
-            content
-        } <
-        /LayoutContainer>, mountNode)
+ReactDOM.render(<
+LayoutContainer layout = {
+layout
+}
+navbar_items = {
+data.navbar_items
+}
+color_variant = {
+color_variant
+}
+sticky = {
+sticky
+}
+is_front = {
+is_front
+}
+> {
+content
+} <
+/LayoutContainer>, mountNode)
