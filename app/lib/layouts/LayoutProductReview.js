73363d9b378f2262a097f999a1c9df372415189c
bisaiahblodgett <bisaiahblodgett@gmail.com>
2017-02-08 17:02:16 +0100

    reworked product review template header section

--- app/lib/layouts/LayoutProductReview.js
+++ app/lib/layouts/LayoutProductReview.js
@@ -17,39 +17,35 @@ class ProductReview extends React.Component {
   render() {
     return (
       <div className='layout layout-product-review'>
-        <Container fluid classes='panel-1'>
-          <Row>
-            <Col md={9} className='header-image'>
+          <Container fluid classes='panel-1'>
               <Container fluid classes='panel-2' backgroundImage={this.props.data.field_particles_image[0]}></Container>
-            </Col>
-            <Col md={3}>
-              <ArticleCategory name={this.props.data.field_main_topics[0].name} url={this.props.data.field_main_topics[0].url}/>
-              <ArticleTitle {...this.props.data.field_headline}/>
-              <HR/>
-              <ParticleText {...this.props.data.field_particles_text[0]}/>
-              <HR/>
-              <ContributorList data={this.props.data.field_contributors} inline separator=', '/>
-            </Col>
-          </Row>
-          <Row className="text-row">
-            <Col className='text-with-columns first-text'>
-              <ParticleText {...this.props.data.field_particles_text[1]}/>
-            </Col>
-          </Row>
-          <Row>
-            <Col className='col-right' md={6}>
-              <ParticleImage {...this.props.data.field_particles_image[1]}/>
-            </Col>
-            <Col className='col-left' md={6}>
-              <ParticleImage {...this.props.data.field_particles_image[2]}/>
-            </Col>
-          </Row>
-          <Row className="text-row last-text">
-            <Col className='text-with-columns'>
-              <ParticleText {...this.props.data.field_particles_text[3]}/>
-            </Col>
-          </Row>
-        </Container>
+                  <Row>
+                      <Col md={3}>
+                          <ArticleCategory name={this.props.data.field_main_topics[0].name} url={this.props.data.field_main_topics[0].url}/>
+                          <ArticleTitle {...this.props.data.field_headline}/>
+                          <ParticleText {...this.props.data.field_particles_text[0]}/>
+                          <ContributorList data={this.props.data.field_contributors} inline separator=', '/>
+                      </Col>
+              </Row>
+              <Row className="text-row">
+                  <Col className='text-with-columns first-text'>
+                      <ParticleText {...this.props.data.field_particles_text[1]}/>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col className='col-right' md={6}>
+                      <ParticleImage {...this.props.data.field_particles_image[1]}/>
+                  </Col>
+                  <Col className='col-left' md={6}>
+                      <ParticleImage {...this.props.data.field_particles_image[2]}/>
+                  </Col>
+              </Row>
+              <Row className="text-row last-text">
+                  <Col className='text-with-columns'>
+                      <ParticleText {...this.props.data.field_particles_text[3]}/>
+                  </Col>
+              </Row>
+          </Container>
       </div>
     )
   }
