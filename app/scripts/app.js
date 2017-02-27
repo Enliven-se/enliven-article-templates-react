e6edc6ae784b448fbed86bb1241222b89be9fde6
klokie <github@klokie.com>
2017-02-27 12:12:37 +0100

    #app => #react-app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import config from '../lib/config.js'
 
 // React root
-const mountNode = document.getElementById('app')
+const mountNode = document.getElementById('react-app')
 
 if (mountNode) {
     const chrome = (mountNode.className.indexOf('no-chrome') < 0)
