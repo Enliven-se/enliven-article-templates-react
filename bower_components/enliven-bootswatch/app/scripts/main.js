a235054bffcdef99ecd06a250bd8a020e8c3835b
klokie <github@klokie.com>
2017-04-26 16:20:44 +0200

    deployed by npm

--- bower_components/enliven-bootswatch/app/scripts/main.js
+++
@@ -1,4 +0,0 @@
-$ = jQuery = require('../../bower_components/jquery/dist/jquery.min.js');
-var bootstrap = require('../../bower_components//bootstrap-sass/assets/javascripts/bootstrap.js');
-
-require('./custom.js');
